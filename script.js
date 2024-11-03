66d7c0cef275b0fddc95802e99c120a915481b94
pperngkam <144779836+pperngkam@users.noreply.github.com>
2024-11-03 15:47:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Nav toggle*/
+document.querySelector(".menu-toggle").addEventListener("click", function () {
+    document.querySelector(".nav-link").classList.toggle("active");
+});
+
+/*Logo alert*/
+function showFunFact() {
+    alert("Did you know? I love coding and learning new technologies!");
+}
